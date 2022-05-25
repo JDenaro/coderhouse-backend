8f9d715e13d17b47ec8ccb4e6c96f40873d31530
Julian Denaro <julian.denaro@teracode.com>
2022-05-25 19:56:17 -0300

    creando objeto desde clase

--- 01-clases/ApellidoNombre.js
+++ 01-clases/ApellidoNombre.js
@@ -1,23 +1,45 @@
 class Usuario {
     constructor(nombre, apellido, libros, mascotas) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros,
-        this.mascotas = mascotas;
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.libros = libros,
+      this.mascotas = mascotas;
     }
+  
     getFullName() {
-        return `${this.nombre + " " + this.apellido}`
+      return `${this.nombre} ${this.apellido}`;
     }
-    AddMascota(mascota) {
-        this.mascotas.push(mascota)
+  
+    addMascota(nombre) {
+      this.mascotas.push(nombre);
     }
+  
     countMascotas() {
-        return (this.mascotas.length)
+      return this.mascotas.length;
     }
-    addBook(nombre, autor) {
-        this.libros.push({ nombreLibro, autor })
+  
+    addBook(nombreLibro, autor) {
+      this.libros.push({ nombreLibro, autor });
     }
-    getBookNames(){
-        return this.libros.map((e) => e.nombreLibro)
+  
+    getBookNames() {
+      return this.libros.map((e) => e.nombreLibro);
     }
-}
+  }
+  
+  let julian = new Usuario(
+    'Julian',
+    'Denaro',
+    [
+      { nombreLibro: 'The DevOps Handbook', autor: 'Gene Kim' },
+      { nombreLibro: 'Site Reliability Engineering: How Google Runs Production Systems', autor: 'Betsy Beyer' },
+    ],
+    ['Perritou']
+  );
+  
+  julian.getFullName();
+  julian.addMascota('Gatitou');
+  julian.countMascotas();
+  julian.addBook('Harry Potter', 'J. K. Rowling');
+  julian.getBookNames();
+  console.log(julian);
